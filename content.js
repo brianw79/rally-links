7babaccda723665c1c3879117bb565b8f25c1876
Brian Whittington <bwhittington@ebsco.com>
2020-03-10 20:03:24 -0500

    using callbacks

--- content.js
+++ content.js
@@ -8,26 +8,57 @@ document.addEventListener('contextmenu', function (e) {
 
   if (srcElement.nodeName == 'SPAN') {
 
-	pastClipboardData();
-	var githubLink = getClipBoardData();
+	  //pastClipboardData(getClipBoardData);
+	  var helperdiv = document.createElement('div');
+	  helperdiv.setAttribute("id", "paste-container");
+	  document.body.appendChild(helperdiv);
+	  helperdiv.contentEditable = true;
 
-	console.log('value from getClipBoardData(): ' + githubLink);
+	  // focus the helper div's content
+	  var range = document.createRange();
+	  range.selectNode(helperdiv);
+	  window.getSelection().removeAllRanges();
+	  window.getSelection().addRange(range);
+	  helperdiv.focus();
 
-	var objectToSave = { 
-		linkText: $(srcElement).text(), 
-		url: protocol + '//' + host + '/' + $(srcElement).parent('a').attr('href').replace(/(\r\n|\n|\r)/gm), 
-		description: $(srcElement).parents('td').siblings('th').find('span')[0].innerText.replace(/(\r\n|\n|\r)/gm,""),
-		copiedGithubLink: githubLink
-	}; 
+	  if (document.execCommand("Paste")) {
+		  var copiedValue = $('#paste-container').children().html();
+		  $('#paste-container').remove();
+		  if (copiedValue && copiedValue.includes('github.com')) {
+			  let store = {};
+			  store['copiedGithubLink'] = copiedValue;
+			  chrome.storage.local.set(store, function () {
+				  var clipBoardData;
+				  let store = {};
+				  store['copiedGithubLink'] = '';
+				  chrome.storage.local.get(store, function (result) {
+					  clipBoardData = result.copiedGithubLink
+					  console.log('Value from chrome.storage.local.get currently is ' + result.copiedGithubLink);
 
-	chrome.runtime.sendMessage({
-		type: 'copy',
-		text: JSON.stringify(objectToSave)
-	});
+					  var githubLink = 'PASTE_GITHUB_LINK_HERE';
+					  if (clipBoardData) {
+						  githubLink = clipBoardData
+					  }
+
+					  var objectToSave = {
+						  linkText: $(srcElement).text(),
+						  url: protocol + '//' + host + '/' + $(srcElement).parent('a').attr('href').replace(/(\r\n|\n|\r)/gm),
+						  description: $(srcElement).parents('td').siblings('th').find('span')[0].innerText.replace(/(\r\n|\n|\r)/gm, ""),
+						  copiedGithubLink: githubLink
+					  };
+
+					  chrome.runtime.sendMessage({
+						  type: 'copy',
+						  text: JSON.stringify(objectToSave)
+					  });
+				  });
+			  });
+		  }
+	  } 
   }
 }, false);
 
-var pastClipboardData  = () => {
+var pastClipboardData = (pastClipboardDataSuccess) => {
 	var helperdiv = document.createElement('div');
 	helperdiv.setAttribute("id", "paste-container");
 	document.body.appendChild(helperdiv);
@@ -45,8 +76,11 @@ var pastClipboardData  = () => {
 		if (copiedValue && copiedValue.includes('github.com')) {
 			let store = {};
 			store['copiedGithubLink'] = copiedValue;
-			chrome.storage.local.set(store, function () {
+			chrome.storage.local.set(store, function () {				
 				console.log('copiedGithubLink set to ' + copiedValue);
+				console.log('calling pastClipboardDataSuccess');
+
+				pastClipboardDataSuccess(storeResult);
 			});					
 		}
 	}
@@ -54,21 +88,39 @@ var pastClipboardData  = () => {
 	$('#paste-container').remove();
 }
 
-var getClipBoardData = () => {
+var getClipBoardData = (getClipBoardDataSuccess) => {
+	console.log('inside getClipBoardData');
+	console.log("getClipBoardDataSuccess is " + getClipBoardDataSuccess)
 	var clipBoardData;
 
 	let store = {};
 	store['copiedGithubLink'] = ''; 
 	chrome.storage.local.get(store, function (result) {
 		clipBoardData = result.copiedGithubLink
-		console.log('Value from get currently is ' + result.copiedGithubLink);
+		console.log('Value from chrome.storage.local.get currently is ' + result.copiedGithubLink);
 
 		var githubLink = 'PASTE_GITHUB_LINK_HERE';
 		if (clipBoardData) {
 			githubLink = clipBoardData
 		}
 
-		return githubLink;
+		getClipBoardDataSuccess(githubLink);
+	});
+}
+
+var storeResult = (githubLink) => {
+	console.log('value from getClipBoardData(): ' + githubLink);
+
+	var objectToSave = {
+		linkText: $(srcElement).text(),
+		url: protocol + '//' + host + '/' + $(srcElement).parent('a').attr('href').replace(/(\r\n|\n|\r)/gm),
+		description: $(srcElement).parents('td').siblings('th').find('span')[0].innerText.replace(/(\r\n|\n|\r)/gm, ""),
+		copiedGithubLink: githubLink
+	};
+
+	chrome.runtime.sendMessage({
+		type: 'copy',
+		text: JSON.stringify(objectToSave)
 	});
 }
 
